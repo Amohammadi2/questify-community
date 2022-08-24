b5190550fb442d15d64629422c334ac6f0ef229d
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2022-08-24 14:10:30 +0430

    [feat] add `/school-questions` page

---
+++ questify-client/pages/school-questions.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const SchoolQuestion: NextPage = ( ) => {
+  return <h1>اینجا لیست سوالات پرسیده شده در مدرسه را خواهید دید</h1>
+}
+
+export default SchoolQuestion;
