6b33995374ce8629c49409b9a0bceb8385a8d8c1
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2023-01-08 10:21:56 +0330

    [ui:registration] add a little bit of padding to success modal

--- questify-client/modules/registration/components/SuccessModal.tsx
+++ questify-client/modules/registration/components/SuccessModal.tsx
@@ -12,9 +12,13 @@ export const SuccessModal = ({ open, onClose } : ISuccessModalProps) => {
     <Modal
       open={open}
       onClose={onClose}
+      css={{
+        py: '$5',
+        px: '$8'
+      }}
     >
-      <Text h3>درخواست شما ثبت شد</Text>
-      <Text>شما به صفحه اصلی وبسایت منتقل خواهید شد</Text>
+      <Text h3 css={{ py: '$4' }}>درخواست شما ثبت شد</Text>
+      <Text css={{ py: '$4'}}>شما به صفحه اصلی وبسایت منتقل خواهید شد</Text>
       <FlexRow css={{ py: '$3' }}>
         <Button color="success" css={{ flexGrow: 1 }} onClick={()=>onClose()}>تایید</Button>
       </FlexRow>
