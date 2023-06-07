7f9639592df99e904f21ba60aef8bb4479c68530
Amohammadi2 <mohammadiashkan1384@gmail.com>
2023-06-07 18:08:08 +0330

    [feat] add publish buttons and title field

--- questify_client/src/routes/AskQuestionPage.tsx
+++ questify_client/src/routes/AskQuestionPage.tsx
@@ -3,7 +3,9 @@ import { Editor } from "react-draft-wysiwyg";
 import { EditorState } from 'draft-js';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "@/styles/editor.css";
-import { Container } from "@mui/material";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -12,7 +14,8 @@ export default function AskQuestionPage() {
   const [editorState, setEditorState] = useState<EditorState>(EditorState.createEmpty())
   
   return (
-    <Container sx={{ height: '500px' }}>
+    <Container sx={{ height: '500px', mt: 2 }}>
+      <TextField label="عنوان سوال" sx={{ width: '100%' }} inputProps={{ style: { fontSize: 30 }}} InputLabelProps={{ style: { top: '5px' } }}/>
       <Editor
         editorState={editorState}
         toolbarClassName="rdw-toolbar"
@@ -20,6 +23,18 @@ export default function AskQuestionPage() {
         editorClassName="rdw-editor"
         onEditorStateChange={setEditorState}
       />
+      <Grid container sx={{ mt: 2 }}>
+        <Button variant="outlined" color="primary" sx={{ mr: 1}}>
+          <Typography sx={{ mr: 1 }}>انتشار سوال</Typography>
+          <FontAwesomeIcon
+            icon={faCheck} />
+        </Button>
+        <Button variant="outlined" color="error" sx={{ mr: 1}}>
+          <Typography sx={{ mr: 1 }}>انصراف</Typography>
+          <FontAwesomeIcon
+              icon={faTimes} />
+        </Button>
+      </Grid>
     </Container>
   )
 }
